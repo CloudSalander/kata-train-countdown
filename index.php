85af71285c8946406d176cbdd50cddca361594dd
Cloud <ruben.as532@gmail.com>
2024-03-14 10:45:48 +0100

    Well defined constants, solved english problem and main game loop added

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
+define("FIRST_BRAKE_CODE", 67);
+define("SECOND_BRAKE_CODE", 81);
 
-define(FIRST_BREAK_CODE, 67);
-define(SECOND_BREAK_CODE, 81);
+function unlockEmergencyBrake(bool $show_partial_sum = true, int $break_code = FIRST_BRAKE_CODE) {
+    return false;
+}
 
-
-
+while(!unlockEmergencyBrake(true,FIRST_BRAKE_CODE)) {
+    echo "AAAAHHH!!".PHP_EOL;
+}
 
 ?>
