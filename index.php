a8748fc25fc54910805b9747067de690c67f88a7
Cloud <ruben.as532@gmail.com>
2024-03-14 10:57:23 +0100

    Added sum and unlock brake condition

--- index.php
+++ index.php
@@ -10,12 +10,17 @@ function unlockEmergencyBrake(bool $show_partial_sum = true, int $break_code = F
     can only have 1 to 9 values*/
     while($numbers_entered < INPUTS_QTY) {
         $number = (int) readline();
+        $sum += $number;
         ++ $numbers_entered;
     }
+    if($sum == $break_code) return true;
+    return false;
 }
 
-while(!unlockEmergencyBrake(true,FIRST_BRAKE_CODE)) {
-    echo "AAAAHHH!!".PHP_EOL;
+$brake_unlocked = false;
+while(!$brake_unlocked) {
+    echo "AAAAHHH!! We're going to die!!!".PHP_EOL;
+    $brake_unlocked = unlockEmergencyBrake(true,FIRST_BRAKE_CODE);
 }
-
+echo "UUFF!! So close...";
 ?>
