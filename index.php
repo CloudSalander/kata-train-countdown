c08f466705cc731ff438b3ec5dda8f6adfa0e252
Cloud <ruben.as532@gmail.com>
2024-03-14 10:58:54 +0100

    Now showing partial sum

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ define("FIRST_BRAKE_CODE", 67);
 define("SECOND_BRAKE_CODE", 81);
 define("INPUTS_QTY",10);
 
-function unlockEmergencyBrake(bool $show_partial_sum = true, int $break_code = FIRST_BRAKE_CODE) {
+function unlockEmergencyBrake(bool $show_partial_sum = true, int $brake_code = FIRST_BRAKE_CODE) {
     $sum = 0;
     $numbers_entered = 0;
     /*Due to the problem statement, we can assume that we 
@@ -11,9 +11,10 @@ function unlockEmergencyBrake(bool $show_partial_sum = true, int $break_code = F
     while($numbers_entered < INPUTS_QTY) {
         $number = (int) readline();
         $sum += $number;
+        if($show_partial_sum) echo $sum."/".$brake_code.PHP_EOL;
         ++ $numbers_entered;
     }
-    if($sum == $break_code) return true;
+    if($sum == $brake_code) return true;
     return false;
 }
 
